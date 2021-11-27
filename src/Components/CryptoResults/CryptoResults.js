c3890b34d6b5eebc1d172ae1eb653a5b58ba745d
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-27 20:48:09 +0100

    Update

--- src/Components/CryptoResults/CryptoResults.js
+++ src/Components/CryptoResults/CryptoResults.js
@@ -1,9 +1,13 @@
 import "./styles.scss";
 
 function CryptoResults() {
+  let numberResulat = 37838967;
+  // let result = numberResulat.toLocaleString();
+  let result = numberResulat.toLocaleString();
+
   return (
     <div className="crypto__container--results">
-      <h3>4999</h3>
+      <h3 className="crypto__container--results-number">{result}</h3>
     </div>
   );
 }
