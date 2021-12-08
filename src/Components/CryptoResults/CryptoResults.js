773ae7a5a51a29cb32a6f3c27b24e20e0cffcbbd
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-08 13:02:28 +0100

    Deploy on netlify

--- src/Components/CryptoResults/CryptoResults.js
+++ src/Components/CryptoResults/CryptoResults.js
@@ -4,11 +4,11 @@ import { DataApiContext } from "../Context/DataContext";
 // import { useState } from "react/cjs/react.development";
 
 function CryptoResults() {
-  const result = useContext(DataApiContext);
   // const { result, currency } = useContext(DataApiContext);
   // const [myResult, setMyResult] = useState(result);
   // const [myCurrency, setMyCurrency] = useState(currency);
 
+  const result = useContext(DataApiContext);
   return (
     <div className="crypto__container--results">
       <h3 className="crypto__container--results-number">{result}</h3>
